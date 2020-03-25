c7b36805f07318ebda0bb99841b248b99ddc60af
Joey Lindsay <61097657+TheJoaster@users.noreply.github.com>
2020-03-25 15:37:30 -0500

    commented saving logic

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -1,50 +1,50 @@
-const storage = require('electron-json-storage');
-const dataPath = '/Users/jbartucz/Downloads/make-a-game-JosephMotherEpic'; // storage.getDefaultDataPath();
-
-let defaultSave = {
-    highscore: 0,
-    currentGame: {
-        columns: 0,
-        row: [{ blockid: 0 }, { blockid: 0 }, { blockid: 0 }, { blockid: 0 }, { blockid: 0 }, { blockid: 0 }, { blockid: 0 }, { blockid: 0 }, { blockid: 0 }]
-    }
-};
+//const storage = require('electron-json-storage');
+//const dataPath = storage.getDefaultDataPath();
+//let data = {};
+//let defaultSave = {
+//    highscore: 0,
+//    currentGame: {
+//        columns: 0,
+//        row: [{ blockid: 0 }, { blockid: 0 }, { blockid: 0 }, { blockid: 0 }, { blockid: 0 }, { blockid: 0 }, { blockid: 0 }, { blockid: 0 }, { blockid: 0 }]
+//    }
+//};
 function makeCells() {
 
 }
-
-function dataReturner(mode) {
-    storage.setDataPath(dataPath);
-    let data = {};
-    switch (mode) {
-        case 'video':
-            storage.get('videosettings', (error, dat) => {
-                if (error) throw error;
-                data = dat;
-                console.log("dat: ")
-                console.log(dat); //returns desired output but still contains __proto__ object.
-                console.log("data: ");
-                console.log(data); //returns desired output but still contains __proto__ object.
-            });
-            console.log(data); //returns __proto__ object... Why tho?
-            return data;
-    }
+function choseBlock(){
+    
 }
-function dataSaver(data, mode) {
-    storage.setDataPath(dataPath);
-    switch (mode) {
-        case 'video':
-
-    }
-}
-let gameRows = {
-    width: 10,
-    height: 20
-};
-let settings = dataReturner('video');
+//function dataReturner(mode) {
+//    storage.setDataPath(dataPath);
+//    
+//    switch (mode) {
+//        case 'video':
+//            
+//            storage.get('videosettings', (error, dat) => {
+//                if (error) throw error;
+//                data = dat;
+//                console.log(dat); //returns desired output but still contains __proto__ object.
+//                
+//                console.log(data); //returns desired output but still contains __proto__ object.
+//                
+//            });
+//            return data;
+            
+//}
+//}
+//function dataSaver(data, mode) {
+//    storage.setDataPath(dataPath);
+//    switch (mode) {
+//        case 'video':
+//
+//    }
+//}
+//let settings = dataReturner('video');
+//console.log(settings);
 let config = { //phaser configuration
     type: Phaser.AUTO,
-    width: settings.width,
-    height: settings.height,
+    width: 800,
+    height: 600,
     scene: {
         preload: preload,
         create: create,
@@ -56,33 +56,12 @@ let game = new Phaser.Game(config);
 
 function preload() {
 
-    console.log("JB1");
-    storage.set('foobar', { foo: 'bar' }, function (error) {
-        if (error) throw error;
-    });
-
-    console.log("JB2");
-    storage.has('foobar', function (error, hasKey) {
-        if (error) throw error;
-
-        if (hasKey) {
-            console.log('There is data stored as `foobar`');
-        } else {
-            console.log('There is NO data stored as `foobar`');
-        }
-    });
-
-    console.log("JB3");
-    storage.get('foobar', function (error, data) {
-        if (error) throw error;
-
-        console.log(data);
-    });
-    console.log("JB4");
 }
+
 function create() {
 
 }
+
 function update() {
 
 }
